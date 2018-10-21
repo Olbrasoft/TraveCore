9fe01e7be6fa4dc67a0c6b47612ad9c0ae0aa252
Jiri Tuma <Olbrasoft@mail.tj>
2018-10-21 20:35:43 +0200

    Create project Olbrasoft.Travel.Expedia.Affiliate.Network.Import

---
+++ src/Olbrasoft.Travel.Expedia.Affiliate.Network.Import/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Olbrasoft.Travel.Expedia.Affiliate.Network.Import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
