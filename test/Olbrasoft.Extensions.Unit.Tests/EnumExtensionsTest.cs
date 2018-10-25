3cfd24d80098c3571ce34455aa86622411e57e70
Jiri Tuma <Olbrasoft@mail.tj>
2018-10-25 17:34:47 +0200

    Create project Olbrasoft.Extensions.Unit.Tests

---
+++ test/Olbrasoft.Extensions.Unit.Tests/EnumExtensionsTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+
+namespace Olbrasoft.Extensions.Unit.Tests
+{
+    [TestFixture]
+    internal class EnumExtensionsTest
+    {
+
+        [Test]
+        public void GetDescription()
+        {
+            //Arrange
+            const string description = "Description of SomeEnumItem";
+
+            //Act
+            var result = SomeEnum.SomeEnumItem.GetDescription();
+
+
+            //Assert
+            Assert.IsTrue(result==description);
+        }
+            
+
+
+    }
+}
---
+++ test/Olbrasoft.Extensions.Unit.Tests/SomeEnum.cs
@@ -0,0 +1,11 @@
+﻿using  System.ComponentModel;
+
+namespace Olbrasoft.Extensions.Unit.Tests
+{
+    enum SomeEnum
+    {
+        [Description("Description of SomeEnumItem")]
+        SomeEnumItem,
+
+    }
+}
